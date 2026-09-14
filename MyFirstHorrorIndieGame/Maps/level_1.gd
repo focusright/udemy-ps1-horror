eb9c029f4dbb405007ad55a072abaebd425e6730
Mike Lee <rockesepcialee@gmail.com>
2026-09-13 22:21:10 -0700

    2. Rotate the player's camera

--- MyFirstHorrorIndieGame/Maps/level_1.gd
+++ MyFirstHorrorIndieGame/Maps/level_1.gd
@@ -3,8 +3,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$RigidBody3D/MeshInstance3D.scale = Vector3(3,3,3)
-	print($RigidBody3D/MeshInstance3D.scale)
+	pass
+	#$RigidBody3D/MeshInstance3D.scale = Vector3(3,3,3)
+	#print($RigidBody3D/MeshInstance3D.scale)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ MyFirstHorrorIndieGame/Player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody3D
+
+@onready var camera_3d = $Camera3D
+var mouse_sens = 0.15
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$MeshInstance3D.visible = false
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		camera_3d.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		
